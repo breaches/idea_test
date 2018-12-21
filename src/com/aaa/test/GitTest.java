d7b344a89cf2a788661ac1a2002bb9aa47d2def8
shaokang <1206751678@qq.com>
2018-12-21 21:56:59 +0800

    testaa commit

--- src/com/aaa/test/GitTest.java
+++ src/com/aaa/test/GitTest.java
@@ -10,4 +10,8 @@ public class GitTest {
     public static void main(String[] args) {
         System.out.println("123");
     }
+
+    public void testaa() {
+        System.out.println("aaa");
+    }
 }
