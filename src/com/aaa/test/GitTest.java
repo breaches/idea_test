c67513af209e67693b78442eb34b62dc82cd48a1
shaokang <1206751678@qq.com>
2018-12-21 21:51:57 +0800

    init commit

---
+++ src/com/aaa/test/GitTest.java
@@ -0,0 +1,13 @@
+package com.aaa.test;
+
+/**
+ * @program: git_test
+ * @description: a
+ * @author: shaokang
+ * @create: 2018-12-21 21:48
+ **/
+public class GitTest {
+    public static void main(String[] args) {
+        System.out.println("123");
+    }
+}
