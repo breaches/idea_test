f013852057c49caec946722a773ff3fef5819986
shaokang <1206751678@qq.com>
2018-12-22 11:17:53 +0800

    dev 测试提交到 dev 分支

---
+++ src/com/aaa/controller/dev/Dev.java
@@ -0,0 +1,13 @@
+package com.aaa.controller.dev;
+
+/**
+ * @program: idea_test
+ * @description:
+ * @author: shaokang
+ * @create: 2018-12-22 11:11
+ **/
+public class Dev {
+    public static void main(String[] args) {
+        System.out.println("asd");
+    }
+}
