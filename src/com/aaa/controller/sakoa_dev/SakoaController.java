222e1132cf696b334b84f7bcddb6e5908fa8b9a2
sakoa <1687868585@qq.com>
2018-12-22 12:53:18 +0800

    sakoa_dev -> SakoaController test

---
+++ src/com/aaa/controller/sakoa_dev/SakoaController.java
@@ -0,0 +1,11 @@
+package com.aaa.controller.sakoa_dev;
+
+/**
+ * @author: H
+ * @create: 2018/12/22 - 12:52
+ */
+public class SakoaController {
+    public static void main(String[] args){
+        System.out.println("adsf");
+    }
+}
