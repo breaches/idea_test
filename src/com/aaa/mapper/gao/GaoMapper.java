509143766c399680146b3ddea7041ff2c6d6ce6f
gaolei <1308859230@qq.com>
2018-12-22 10:04:27 +0800

    gaolei 测试

---
+++ src/com/aaa/mapper/gao/GaoMapper.java
@@ -0,0 +1,7 @@
+package com.aaa.mapper.gao;
+
+public class GaoMapper {
+    public static void main(String[] args) {
+        System.out.println("order");
+    }
+}
---
+++ src/com/aaa/service/GAo/GaoSerivice.java
@@ -0,0 +1,7 @@
+package com.aaa.service.GAo;
+
+public class GaoSerivice {
+    public static void main(String[] args) {
+        System.out.println("order");
+    }
+}
